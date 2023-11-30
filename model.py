6153162a7fad0dc4f884820056ac186045f974a6
NLP <33448441+yangzhipeng1108@users.noreply.github.com>
2023-11-30 08:30:56 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,15 +1,14 @@
 # -*- coding:utf-8 -*-
-
 """
     文件说明：
             
 """
-from glm2.modeling_chatglm import ChatGLMForConditionalGeneration as ChatGLM2ForConditionalGeneration
-from glm2.tokenization_chatglm import ChatGLMTokenizer as ChatGLM2Tokenizer
-from glm2.configuration_chatglm import ChatGLMConfig as ChatGLM2Config
-from glm1.modeling_chatglm import ChatGLMForConditionalGeneration
-from glm1.tokenization_chatglm import ChatGLMTokenizer
-from glm1.configuration_chatglm import ChatGLMConfig
+# from glm2.modeling_chatglm import ChatGLMForConditionalGeneration as ChatGLM2ForConditionalGeneration
+# from glm2.tokenization_chatglm import ChatGLMTokenizer as ChatGLM2Tokenizer
+# from glm2.configuration_chatglm import ChatGLMConfig as ChatGLM2Config
+# from glm1.modeling_chatglm import ChatGLMForConditionalGeneration
+# from glm1.tokenization_chatglm import ChatGLMTokenizer
+# from glm1.configuration_chatglm import ChatGLMConfig
 from utils import GLMPromptDataSet, GLM2PromptDataSet
 from transformers import (
     AutoConfig,
@@ -17,10 +16,12 @@ from transformers import (
     AutoTokenizer,
 )
 
-MODE = {"glm": {"model": ChatGLMForConditionalGeneration, "tokenizer": ChatGLMTokenizer, "config": ChatGLMConfig,
-                "dataset": GLMPromptDataSet},
-        "glm2": {"model": ChatGLM2ForConditionalGeneration, "tokenizer": ChatGLM2Tokenizer, "config": ChatGLM2Config,
-                 "dataset": GLM2PromptDataSet},
+MODE = {
+       # "glm": {"model": ChatGLMForConditionalGeneration, "tokenizer": ChatGLMTokenizer, "config": ChatGLMConfig,
+       #          "dataset": GLMPromptDataSet},
+       #  "glm2": {"model": ChatGLM2ForConditionalGeneration, "tokenizer": ChatGLM2Tokenizer, "config": ChatGLM2Config,
+       #           "dataset": GLM2PromptDataSet},
         "baichuan2": {"model": AutoModelForCausalLM, "tokenizer": AutoTokenizer, "config": AutoConfig,
-                 "dataset": GLM2PromptDataSet}}
-
+                 "dataset": GLM2PromptDataSet},
+        "yi2": {"model": AutoModelForCausalLM, "tokenizer": AutoTokenizer, "config": AutoConfig,
+                      "dataset": GLM2PromptDataSet}}
